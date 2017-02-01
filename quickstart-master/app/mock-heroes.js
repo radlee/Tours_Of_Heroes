30391438dbac63964f41982e26aa1289629ed923
radlee <lolito@projectcodex.co>
2017-02-01 07:07:29 +0200

    First Commit

---
+++ quickstart-master/app/mock-heroes.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.HEROES = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magenta' },
+    { id: 16, name: 'Tornado' },
+];
+//# sourceMappingURL=mock-heroes.js.map
